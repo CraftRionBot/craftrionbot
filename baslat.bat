cb3629c636b1e3dd811ab743ec325aad675c79a0
CraftRionBot <54190101+CraftRionBot@users.noreply.github.com>
2019-08-18 23:14:24 +0300

    sad
    sad

---
+++ baslat.bat
@@ -0,0 +1,6 @@
+encho off
+color 0c
+cls
+:a
+node bot.js
+goto a
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Selamlar,Değerli **' + username + '** Craft Rion resmi discord sunucusuna hoşgeldiniz :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle https://media.giphy.com/media/hXCMLIhPMUTSkBHixD/giphy.gif :wink:');
+
+};
